28ffb95c2f2e037fa8b931e46e922cdc8cbfee69
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-11 10:02:28 +0100

    Agregar archivos de proyecto.

---
+++ Hangman/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class Hangman
+    {
+        static void Main(string[] args)
+        {
+            Hangman game = new Hangman();
+            game.run();
+
+            Console.ReadLine();
+        }
+
+        private void run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
