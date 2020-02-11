b3ca92d45190c643ebc1de6b1d865a0d8af2d53f
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-02-11 11:25:55 +0100

    box class constructor and mine

--- Minesweeper/Box.cs
+++ Minesweeper/Box.cs
@@ -2,15 +2,23 @@
 
 namespace Minesweeper
 {
+  
+
     internal class Box
     {
+        private bool mine;
+        private int value;
+        private bool sweeped;
         public Box()
         {
+            mine = false;
+            value = 0;
+            sweeped = true;
         }
 
         internal void addMine()
         {
-            throw new NotImplementedException();
+            mine = true;
         }
     }
 }
